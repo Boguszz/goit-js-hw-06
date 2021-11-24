7e1f4fa993e42a0d545a212cfa0d3bfcd36e7163
Bohdan Strilets <bohdan.strilets@gmail.com>
2021-11-24 10:38:47 +0100

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -16,21 +16,34 @@
 // Category: Technologies
 // Elements: 5
 
-// ============================== Solution ==============================
+// ============================== Solution - 1 ==============================
 
-const categoriesRef = document.querySelectorAll(".item");
-const allCategoryTitlesRef = document.querySelectorAll("h2");
-const animalsRef = categoriesRef[0].querySelectorAll("li");
-const productsRef = categoriesRef[1].querySelectorAll("li");
-const technologiesRef = categoriesRef[2].querySelectorAll("li");
+// const categoriesRef = document.querySelectorAll(".item");
+// const allCategoryTitlesRef = document.querySelectorAll("h2");
+// const animalsRef = categoriesRef[0].querySelectorAll("li");
+// const productsRef = categoriesRef[1].querySelectorAll("li");
+// const technologiesRef = categoriesRef[2].querySelectorAll("li");
 
-console.log(`Number of categories: ${categoriesRef.length}`); // Number of categories: 3
-console.log("");
-console.log(`Category: ${allCategoryTitlesRef[0].textContent}`); // Category: Animals
-console.log(`Elements: ${animalsRef.length}`); // Elements: 4
-console.log("");
-console.log(`Category: ${allCategoryTitlesRef[1].textContent}`); // Category: Products
-console.log(`Elements: ${productsRef.length}`); // Elements: 3
-console.log("");
-console.log(`Category: ${allCategoryTitlesRef[2].textContent}`); // Category: Technologies
-console.log(`Elements: ${technologiesRef.length}`); // Elements: 5
+// console.log(`Number of categories: ${categoriesRef.length}`); // Number of categories: 3
+// console.log("");
+// console.log(`Category: ${allCategoryTitlesRef[0].textContent}`); // Category: Animals
+// console.log(`Elements: ${animalsRef.length}`); // Elements: 4
+// console.log("");
+// console.log(`Category: ${allCategoryTitlesRef[1].textContent}`); // Category: Products
+// console.log(`Elements: ${productsRef.length}`); // Elements: 3
+// console.log("");
+// console.log(`Category: ${allCategoryTitlesRef[2].textContent}`); // Category: Technologies
+// console.log(`Elements: ${technologiesRef.length}`); // Elements: 5
+
+// ============================== Solution - 2 ==============================
+
+const allCategoriesRefs = document.querySelectorAll(".item");
+
+console.log(`Number of categories: ${allCategoriesRefs.length}`);
+allCategoriesRefs.forEach((item) => {
+  const sublist = item.querySelectorAll("ul li");
+  const title = item.querySelector("h2");
+  console.log("");
+  console.log(`Category: ${title.textContent}`);
+  console.log(`Elements: ${sublist.length}`);
+});
