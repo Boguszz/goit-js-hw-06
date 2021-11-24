5057bc05da5ed480deac9303ad2e772c38a6041e
Bohdan Strilets <bohdan.strilets@gmail.com>
2021-11-24 13:06:36 +0100

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -2,20 +2,40 @@
 // через инлайн стиль при клике на button.change-color и выводит значение цвета в span.color.
 // Для генерации случайного цвета используй функцию getRandomHexColor.
 
-// ============================== Solution ==============================
+// ============================== Solution - 1 ==============================
+
+// const refs = {
+//   chengeColor: document.querySelector(".change-color"),
+//   selectedColor: document.querySelector(".color"),
+// };
+
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// }
+// console.log(getRandomHexColor());
+
+// function onColorChange(event) {
+//   document.body.style.backgroundColor = getRandomHexColor();
+//   refs.selectedColor.textContent = getRandomHexColor();
+// }
+
+// refs.chengeColor.addEventListener("click", onColorChange);
+
+// ============================== Solution - 2 ==============================
 
 const refs = {
   chengeColor: document.querySelector(".change-color"),
   selectedColor: document.querySelector(".color"),
 };
 
+refs.chengeColor.addEventListener("click", onColorChange);
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
 function onColorChange(event) {
-  document.body.style.backgroundColor = getRandomHexColor();
-  refs.selectedColor.textContent = getRandomHexColor();
+  const color = getRandomHexColor();
+  document.body.style.backgroundColor = color;
+  refs.selectedColor.textContent = color;
 }
-
-refs.chengeColor.addEventListener("click", onColorChange);
