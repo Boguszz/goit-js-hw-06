0dcba5bed8a7013741ceb0007219e4700b806fc4
Bohdan Strilets <bohdan.strilets@gmail.com>
2021-11-24 11:28:49 +0100

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -4,7 +4,33 @@
 // Добавит элементу класс item.
 // После чего вставит все <li> за одну операцию в список ul.ingredients.
 
-// ============================== Solution ==============================
+// ============================== Solution - 1 ==============================
+
+// const ingredients = [
+//   "Potatoes",
+//   "Mushrooms",
+//   "Garlic",
+//   "Tomatos",
+//   "Herbs",
+//   "Condiments",
+// ];
+
+// const layoutGeneratorForIngredientsList = (ingredient) => {
+//   const ingredientsRef = document.querySelector("#ingredients");
+
+//   const itemRef = document.createElement("li");
+//   itemRef.classList.add("item");
+//   itemRef.textContent = ingredient;
+
+//   ingredientsRef.append(itemRef);
+//   return ingredientsRef;
+// };
+
+// const createMarkupOnPage = ingredients.map((ingredient) =>
+//   layoutGeneratorForIngredientsList(ingredient)
+// );
+
+// ============================== Solution - 2 ==============================
 
 const ingredients = [
   "Potatoes",
@@ -15,17 +41,14 @@ const ingredients = [
   "Condiments",
 ];
 
-const layoutGeneratorForIngredientsList = (ingredient) => {
+(function createMarkupIngredients() {
   const ingredientsRef = document.querySelector("#ingredients");
 
-  const itemRef = document.createElement("li");
-  itemRef.classList.add("item");
-  itemRef.textContent = ingredient;
-
-  ingredientsRef.append(itemRef);
-  return ingredientsRef;
-};
-
-const createMarkupOnPage = ingredients.map((ingredient) =>
-  layoutGeneratorForIngredientsList(ingredient)
-);
+  const markupForItems = ingredients.map((ingredient) => {
+    const itemRef = document.createElement("li");
+    itemRef.classList.add("item");
+    itemRef.textContent = ingredient;
+    return ingredientsRef.append(itemRef);
+  });
+  return markupForItems;
+})();
